9b6f5fcad3f6b1f565726ac0f9cb2ad47b19e46c
wlrff <vvlrff@gmail.com>
2024-01-17 22:50:09 +0300

    /

--- database.py
+++ database.py
@@ -1,44 +1,37 @@
 import logging
-from sqlalchemy import create_engine, Column, BigInteger, MetaData, Table, select
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+import asyncpg
 from config import POSTGRES_USER, POSTGRES_PASSWORD, DB_HOST, DB_PORT, POSTGRES_DB
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = 'users'
-    id = Column(BigInteger, primary_key=True)
-    chat_id = Column(BigInteger)
 
 class Database:
     def __init__(self):
-        self.engine = create_engine(
-            f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}"
+        self.pool = None
+
+    async def connect(self):
+        self.pool = await asyncpg.create_pool(
+            user=POSTGRES_USER,
+            password=POSTGRES_PASSWORD,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=POSTGRES_DB
         )
-        self.metadata = MetaData()
 
-        # Явное объявление столбцов
-        self.users = Table('users', self.metadata,
-                          Column('id', BigInteger, primary_key=True),
-                          Column('chat_id', BigInteger),
-                          extend_existing=True)
-
-        Base.metadata.create_all(self.engine)
-        self.Session = sessionmaker(bind=self.engine)
+    async def close(self):
+        if self.pool:
+            await self.pool.close()
 
     async def add_chat_id(self, chat_id: int):
         try:
-            with self.Session() as session:
-                user = User(chat_id=chat_id)
-                session.add(user)
-                session.commit()
+            async with self.pool.acquire() as connection:
+                await connection.execute("INSERT INTO users (chat_id) VALUES ($1)", chat_id)
             logging.info(f"Successfully added chat_id {chat_id} to the database.")
         except Exception as e:
             logging.error(f"Error adding chat_id {chat_id} to the database: {e}")
 
     async def get_chat_ids(self):
-        with self.Session() as session:
-            query = select([self.users.c.chat_id])
-            result = session.execute(query)
-            return [row[0] for row in result]
+        try:
+            async with self.pool.acquire() as connection:
+                result = await connection.fetch("SELECT chat_id FROM users")
+            return [row['chat_id'] for row in result]
+        except Exception as e:
+            logging.error(f"Error retrieving chat_ids from the database: {e}")
+            return []
--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import logging
 import random
 import asyncio
-from datetime import datetime, timedelta
 from aiogram import Bot, Dispatcher, types
+from aiogram import executor
 import schedule
 
 from database import Database
 from config import TELEGRAM_TOKEN
 
 logging.basicConfig(level=logging.INFO)
+logging.getLogger("aiogram").setLevel(logging.DEBUG)
+
 bot = Bot(token=TELEGRAM_TOKEN)
 dp = Dispatcher(bot)
 db = Database()
@@ -16,13 +18,20 @@ db = Database()
 
 async def send_message(chat_ids):
     try:
-        with open("text.txt") as file:
+        logging.info(f"Attempting to send message to chat_ids: {chat_ids}")
+
+        if not chat_ids:
+            logging.info(
+                "No chat_ids found in the database. Skipping message sending.")
+            return
+
+        with open("text.txt", encoding="utf-8") as file:
             compliments = [line.strip() for line in file]
             random_compliment = random.choice(compliments)
 
         for chat_id in chat_ids:
             logging.info(f"Sending message to chat_id {chat_id}")
-            await bot.send_message(chat_id, random_compliment)
+            await bot.send_message(chat_id, random_compliment.encode("utf-8").decode("utf-8"))
             logging.info(f"Message sent successfully to chat_id {chat_id}")
 
         logging.info("Messages sent successfully.")
@@ -33,13 +42,23 @@ async def send_message(chat_ids):
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
     chat_id = message.chat.id
+    logging.info(f"тык")
     try:
+        logging.info(f"Received /start command from chat_id {chat_id}")
+
         existing_chat_ids = await db.get_chat_ids()
+        logging.info(f"Existing chat_ids in the database: {existing_chat_ids}")
+
         if chat_id not in existing_chat_ids:
             await db.add_chat_id(chat_id)
             logging.info(f"Added chat_id {chat_id} to the database.")
+
+        # Отправляем дополнительное сообщение
+        await send_message([chat_id])
+        logging.info(f"Additional message sent to chat_id {chat_id}")
     except Exception as e:
         logging.error(f"Error in cmd_start: {e}")
+        raise  # Добавим эту строку для более подробной информации об ошибке
 
 
 async def scheduled_job():
@@ -48,12 +67,43 @@ async def scheduled_job():
 
 
 def job():
-    asyncio.run(scheduled_job())
+    asyncio.create_task(scheduled_job())
 
 
-schedule.every(1).hours.do(job)
+async def on_startup(dp):
+    # Запускаем расписание
+    schedule.every(10).seconds.do(job)
 
-if __name__ == '__main__':
+    # Устанавливаем вебхук, чтобы использовать Long Polling
+    await bot.delete_webhook()
+
+    # Подключаемся к базе данных
+    await db.connect()
+
+    # Добавим логирование для проверки наличия пользователей в базе данных
+    existing_chat_ids = await db.get_chat_ids()
+    logging.info(f"Existing chat_ids in the database: {existing_chat_ids}")
+
+    # Запускаем цикл выполнения задач в фоновом режиме
+    asyncio.create_task(background_task())
+
+
+async def background_task():
     while True:
         schedule.run_pending()
-        asyncio.sleep(1)
+        await asyncio.sleep(1)
+
+
+async def on_shutdown(dp):
+    # Закрываем подключение к базе данных
+    await db.close()
+
+
+def main():
+    # Запускаем бота с использованием Long Polling
+    executor.start_polling(dp, on_startup=on_startup,
+                           on_shutdown=on_shutdown, skip_updates=True)
+
+
+if __name__ == "__main__":
+    main()
